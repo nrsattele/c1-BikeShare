8e759ae6c816d8fc875cc2136092b57780477ffe
Nick Sattele <nrsattele@gmail.com>
2018-10-15 16:14:09 -0400

    display data to screen

--- scripts/data.js
+++ scripts/data.js
@@ -1,10 +1,12 @@
-// Load CSV File as a String
-function runCSV(flag) {
+// Loads Data
+function getData(flag) {
     fetch('data/metro-bike-share-trip-data.csv').then(function (response) {
         if (response.status !== 200) {
             throw response.status;
         }
         console.log(response.text());
+        var myData = document.getElementById('myData');
+        myData.innerText = response.text;
         return response.text();
     }).catch(function (status) {
         console.log('Error ' + status);
