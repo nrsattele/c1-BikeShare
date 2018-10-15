af2d2c74fb7fcf7957c1c8aba18e4ef9bb56f679
Nick Sattele <nrsattele@gmail.com>
2018-10-15 16:38:43 -0400

    new get data

--- scripts/data.js
+++ scripts/data.js
@@ -14,3 +14,17 @@ function getData(flag) {
         console.log('Error ' + status);
     });
 }
+
+function httpGetAsync(theUrl, callback){
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.onreadystatechange = function() { 
+        if (xmlHttp.readyState == 4 && xmlHttp.status == 200)
+            callback(xmlHttp.responseText);
+    }
+    xmlHttp.open("GET", theUrl, true); // true for asynchronous 
+    xmlHttp.send(null);
+}
+
+function callback(respText) {
+    console.log(respText);
+}
